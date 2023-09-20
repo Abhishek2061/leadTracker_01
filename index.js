d88175bbc1ea40b2eeba31670b0dbdd30e5c46a3
Abhishek Chand <114099112+Abhishek2061@users.noreply.github.com>
2023-09-20 21:09:58 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,40 @@
+// function saved() {
+//   console.log("Button clicked");
+// }
+let myLeads = [];
+let oldLeads = [];
+const inputEl = document.getElementById("input-el");
+const inputBtn = document.getElementById("input-btn");
+const ulEl = document.getElementById("ul-el");
+const deleteBtn = document.getElementById("delete-btn");
+// const tabBTn = document.getElementById("tab-btn");
+const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"));
+if (leadsFromLocalStorage) {
+  myLeads = leadsFromLocalStorage;
+  render(myLeads);
+}
+
+function render(leads) {
+  let listItems = "";
+  for (let i = 0; i < leads.length; i++) {
+    listItems += `<li><a target='_blank' href='${leads[i]}'>${leads[i]}
+       </a></li>`;
+  }
+  ulEl.innerHTML = listItems;
+}
+
+deleteBtn.addEventListener("dblclick", function () {
+  let conf = confirm("Are you sure");
+  if (conf == 1) {
+    localStorage.clear();
+    myLeads = [];
+    render(myLeads);
+  }
+});
+
+inputBtn.addEventListener("click", function () {
+  myLeads.push(inputEl.value);
+  inputEl.value = "";
+  localStorage.setItem("myLeads", JSON.stringify(myLeads));
+  render(myLeads);
+});
